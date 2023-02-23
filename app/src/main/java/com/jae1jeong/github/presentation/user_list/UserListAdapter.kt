4482217846f831ffd63108f9ecb9f701adc1441b
jae1jeong <wodnjs3600@naver.com>
2023-02-24 02:05:32 +0900

    feat/userlistadapter 추가 중ㅁ

--- app/src/main/java/com/jae1jeong/github/presentation/user_list/UserListAdapter.kt
+++ app/src/main/java/com/jae1jeong/github/presentation/user_list/UserListAdapter.kt
@@ -2,6 +2,23 @@ package com.jae1jeong.github.presentation.user_list
 
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.jae1jeong.github.domain.model.user_list.UserUiModel
 
-class UserListAdapter {
+class UserListAdapter:PagingDataAdapter<UserUiModel,UserListAdapter.ViewHolder>( DIFF_CALLBACK ) {
+    inner class ViewHolder(private val binding: ): RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<UserUiModel>() {
+            override fun areItemsTheSame(oldItem: UserUiModel, newItem: UserUiModel): Boolean {
+                return oldItem.userId == newItem.userId
+            }
+
+            override fun areContentsTheSame(oldItem: UserUiModel, newItem: UserUiModel): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
 }
