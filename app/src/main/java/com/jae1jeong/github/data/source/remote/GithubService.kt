819945e2af2e5020c4bcab13d42883f7c3ec4a8a
jae1jeong <wodnjs3600@naver.com>
2023-02-23 22:06:38 +0900

    feat/Github service 검색,유저 상세, 유저 레포 상세 추가

--- app/src/main/java/com/jae1jeong/github/data/source/remote/GithubService.kt
+++ app/src/main/java/com/jae1jeong/github/data/source/remote/GithubService.kt
@@ -1,4 +1,21 @@
 package com.jae1jeong.github.data.source.remote
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Github API communication setup via Retrofit.
+ */
 interface GithubService {
+    // search user by keyword
+    @GET("search/users")
+    suspend fun searchUsers(@Query("q") query: String): Nothing
+
+    // user detail
+    @GET("users/{username}")
+    suspend fun getUser(@Query("username") username: String): Nothing
+
+    // user repos
+    @GET("users/{username}/repos")
+    suspend fun getRepos(@Query("username") username: String): Nothing
 }
