4122f1ccaf39d26ff6f85481ac09406f129954c6
jae1jeong <wodnjs3600@naver.com>
2023-02-23 21:49:11 +0900

    feat/networkmodule 레트로핏 provide 추가

--- app/src/main/java/com/jae1jeong/github/di/NetworkModule.kt
+++ app/src/main/java/com/jae1jeong/github/di/NetworkModule.kt
@@ -1,4 +1,24 @@
 package com.jae1jeong.github.di
 
+import com.jae1jeong.github.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
