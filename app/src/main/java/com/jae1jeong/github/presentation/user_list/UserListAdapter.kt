e15f56b0320b87ed0e0698310f4e7507f743ebcb
jae1jeong <wodnjs3600@naver.com>
2023-02-24 02:13:56 +0900

    refactor/user list adapter 수정

--- app/src/main/java/com/jae1jeong/github/presentation/user_list/UserListAdapter.kt
+++ app/src/main/java/com/jae1jeong/github/presentation/user_list/UserListAdapter.kt
@@ -1,13 +1,17 @@
 package com.jae1jeong.github.presentation.user_list
 
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.jae1jeong.github.databinding.ItemUserBinding
 import com.jae1jeong.github.domain.model.user_list.UserUiModel
 
-class UserListAdapter:PagingDataAdapter<UserUiModel,UserListAdapter.ViewHolder>( DIFF_CALLBACK ) {
-    inner class ViewHolder(private val binding: ): RecyclerView.ViewHolder(binding.root) {
-
+class UserListAdapter : PagingDataAdapter<UserUiModel, UserListAdapter.ViewHolder>(DIFF_CALLBACK) {
+    inner class ViewHolder(val binding: ItemUserBinding) :
+        RecyclerView.ViewHolder(binding.root) {
     }
 
     companion object {
@@ -20,5 +24,19 @@ class UserListAdapter:PagingDataAdapter<UserUiModel,UserListAdapter.ViewHolder>(
                 return oldItem == newItem
             }
         }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.user = snapshot()[position]
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemUserBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 }
