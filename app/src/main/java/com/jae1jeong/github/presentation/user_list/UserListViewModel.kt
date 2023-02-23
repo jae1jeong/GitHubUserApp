157f60af4a057766c49fff78ced799cce94306c1
jae1jeong <wodnjs3600@naver.com>
2023-02-24 02:40:39 +0900

    feat/user list viewmodel getUsersByKeyword 추가

--- app/src/main/java/com/jae1jeong/github/presentation/user_list/UserListViewModel.kt
+++ app/src/main/java/com/jae1jeong/github/presentation/user_list/UserListViewModel.kt
@@ -1,10 +1,35 @@
 package com.jae1jeong.github.presentation.user_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.jae1jeong.github.data.source.remote.GithubService
+import com.jae1jeong.github.data.source.remote.UserListPagingSource
+import com.jae1jeong.github.domain.model.user_list.UserUiModel
+import com.jae1jeong.github.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
 @HiltViewModel
-class UserListViewModel @Inject constructor():ViewModel(){
+class UserListViewModel @Inject constructor(
+    private val githubService: GithubService
+):BaseViewModel(){
+
+
+    companion object{
+        const val PAGE_LIMIT = 20
+    }
+
+    fun getUsersByKeyword(keyword:String): Flow<PagingData<UserUiModel>> {
+        _loadingEvent.value   = true
+        return Pager(PagingConfig(PAGE_LIMIT)){
+            _loadingEvent.value = false
+            UserListPagingSource(githubService = githubService, query = keyword)
+        }.flow.cachedIn(viewModelScope)
+    }
 }
