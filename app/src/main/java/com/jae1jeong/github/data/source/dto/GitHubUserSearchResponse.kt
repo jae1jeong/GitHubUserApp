d744cb552071034a2004f9883ef11ca3a965936d
jae1jeong <wodnjs3600@naver.com>
2023-02-23 22:12:43 +0900

    style/github user response item -> user 네이밍 변경

--- app/src/main/java/com/jae1jeong/github/data/source/dto/GitHubUserSearchResponse.kt
+++ app/src/main/java/com/jae1jeong/github/data/source/dto/GitHubUserSearchResponse.kt
@@ -2,12 +2,12 @@ package com.jae1jeong.github.data.source.dto
 
 data class GitHubUserSearchResponse(
     val incomplete_results: Boolean,
-    val items: List<Item>,
+    val items: List<User>,
     val total_count: Int
 )
 
 
-data class Item(
+data class User(
     val avatar_url: String,
     val events_url: String,
     val followers_url: String,
