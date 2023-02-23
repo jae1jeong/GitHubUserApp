519f2e8206b0d8a3051a09abcc860a55278f01c4
jae1jeong <wodnjs3600@naver.com>
2023-02-24 02:51:16 +0900

    user list event edittext event 추가

--- app/src/main/java/com/jae1jeong/github/presentation/user_list/UserListActivity.kt
+++ app/src/main/java/com/jae1jeong/github/presentation/user_list/UserListActivity.kt
@@ -3,9 +3,16 @@ package com.jae1jeong.github.presentation.user_list
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.jae1jeong.github.R
 import com.jae1jeong.github.databinding.ActivityUserlistBinding
 import com.jae1jeong.github.presentation.base.BaseActivity
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import reactivecircus.flowbinding.android.widget.textChanges
 
 class UserListActivity: BaseActivity<ActivityUserlistBinding,UserListViewModel>() {
 
@@ -15,8 +22,27 @@ class UserListActivity: BaseActivity<ActivityUserlistBinding,UserListViewModel>(
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun observeData() {
+
+    }
+
+    override fun setEvent() {
+        binding.etSearchUser.textChanges()
+            .debounce(500)
+            .filter { !it.toString().isNullOrEmpty() }
+            .onEach {
+                lifecycleScope.launch {
+                    viewModel.getUsersByKeyword(it.toString())
+                }
+            }.launchIn(lifecycleScope)
     }
 
 }
