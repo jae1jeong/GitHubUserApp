9f25ed2c6fc9c9cb27f4e96ab6fafcc33e46f182
jae1jeong <wodnjs3600@naver.com>
2023-02-24 14:34:12 +0900

    fix/유저 리스트 검색 리스트가 안 뜨는 현상 수정

--- app/src/main/java/com/jae1jeong/github/presentation/user_list/UserListActivity.kt
+++ app/src/main/java/com/jae1jeong/github/presentation/user_list/UserListActivity.kt
@@ -1,11 +1,16 @@
 package com.jae1jeong.github.presentation.user_list
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.PagingData
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jae1jeong.github.R
 import com.jae1jeong.github.databinding.ActivityUserlistBinding
 import com.jae1jeong.github.presentation.base.BaseActivity
+import com.jae1jeong.github.utlis.decoration.UserListDividerDecoration
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
@@ -31,21 +36,32 @@ class UserListActivity: BaseActivity<ActivityUserlistBinding,UserListViewModel>(
         binding.rvSearchUsers.apply {
             setHasFixedSize(true)
             adapter = userListAdapter
-            addItemDecoration(UserListDecoration())
+            addItemDecoration(UserListDividerDecoration(2))
         }
     }
 
     override fun observeData() {
+        viewModel.loadingEvent.observe(this){
+            if(it){
 
+            }else{
+
+            }
+        }
     }
 
     override fun setEvent() {
         binding.etSearchUser.textChanges()
             .debounce(500)
+            .map {
+                if(it.isNullOrEmpty()){
+                    userListAdapter.submitData(lifecycle, PagingData.empty())
+                }else it
+            }
             .filter { !it.toString().isNullOrEmpty() }
             .onEach {
                 lifecycleScope.launch {
-                    viewModel.getUsersByKeyword(it.toString()).collectLatest {
+                    viewModel.getUsersByKeyword(it.toString()).collect {
                         userListAdapter.submitData(it)
                     }
                 }
--- app/src/main/java/com/jae1jeong/github/presentation/user_list/UserListAdapter.kt
+++ app/src/main/java/com/jae1jeong/github/presentation/user_list/UserListAdapter.kt
@@ -1,18 +1,24 @@
 package com.jae1jeong.github.presentation.user_list
 
 import android.text.Layout
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.jae1jeong.github.R
 import com.jae1jeong.github.databinding.ItemUserBinding
 import com.jae1jeong.github.domain.model.user_list.UserUiModel
 
 class UserListAdapter : PagingDataAdapter<UserUiModel, UserListAdapter.ViewHolder>(DIFF_CALLBACK) {
     inner class ViewHolder(val binding: ItemUserBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-    }
+        RecyclerView.ViewHolder(binding.root){
+            fun onBind(item: UserUiModel){
+                binding.user = item
+                Log.d("TAG", "onBind: $item")
+            }
+        }
 
     companion object {
         private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<UserUiModel>() {
@@ -27,16 +33,16 @@ class UserListAdapter : PagingDataAdapter<UserUiModel, UserListAdapter.ViewHolde
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.user = snapshot()[position]
+        val item = snapshot()[position]
+        item?.let {
+            holder.onBind(it)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-            ItemUserBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_user, parent, false)
+        val binding = ItemUserBinding.bind(view)
+        return ViewHolder(binding)
     }
 }
